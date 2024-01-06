36fd9c14a5e65d052a5c9173d225273c7ce2841f
jcpopdigital <155778536+jcpopdigital@users.noreply.github.com>
2024-01-05 20:43:51 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,20 @@ OS="$(uname)"
 
 # Set the environment variable
 if [[ "${OS}" == "Linux" ]]; then
- echo 'export GITHUB_USERNAME=jcpopdigital' >> /root/.bash_profile
+ echo 'export GITHUB_USERNAME=jcchoi' >> /root/.bash_profile
 elif [[ "${OS}" == "Darwin" ]]; then
- echo 'export GITHUB_USERNAME=jcpopdigital' >> /Users/jcpopdigital/.bash_profile
+ echo 'export GITHUB_USERNAME=jcchoi' >> /Users/jcchoi/.bash_profile
 else
  echo "Unknown OS"
  exit 1
 fi
+
+# Define the HOSTNAME
+HOSTNAME="vault-server.hashicorp.com"
+
+# Write the code to config.hcl
+cat <<EOF > config.hcl
+cluster_addr = "https://${HOSTNAME}:8201"
+api_addr   = "https://${HOSTNAME}:8200"
+disable_mlock = true
+EOF
