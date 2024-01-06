9a26f9efdf780f748e16d1052b1eee13ff02a1ed
jcpopdigital <155778536+jcpopdigital@users.noreply.github.com>
2024-01-05 20:04:49 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Determine the OS
+OS="$(uname)"
+
+# Set the environment variable
+if [[ "${OS}" == "Linux" ]]; then
+ echo 'export GITHUB_USERNAME=<your_username>' >> /root/.bash_profile
+elif [[ "${OS}" == "Darwin" ]]; then
+ echo 'export GITHUB_USERNAME=<your_username>' >> /Users/<your_username>/.bash_profile
+else
+ echo "Unknown OS"
+ exit 1
+fi
+
 # Set the environment variable
 echo 'export GITHUB_USERNAME=user_name_here' >> /root/.bash_profile
